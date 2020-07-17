423b489785cadd06e87783d8daa25afe77784001
Rishabh Das <rishabhdasufl@gmail.com>
2020-07-17 20:52:03 +0530

    Build list

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,11 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  //   console.log(results);
   useEffect(() => {
     const search = async () => {
-      await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
         params: {
           action: "query",
           list: "search",
@@ -14,9 +16,24 @@ const Search = () => {
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
-    search();
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageId} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+          {/* {result.snippet} */}
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -29,6 +46,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
